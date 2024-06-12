9ddb90371a8ab4da68b7b6d633d80ce1430451fd
vigneshraja122 <vraja3868@gmail.com>
2024-06-12 16:56:11 +0530

    Added action button to the datatable

--- src/app/(all)/folders/columns.tsx
+++ src/app/(all)/folders/columns.tsx
@@ -1,13 +1,22 @@
-"use client"
- 
-import { ColumnDef } from "@tanstack/react-table"
- 
+"use client";
+import { ColumnDef } from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { HiOutlineDotsVertical } from "react-icons/hi";
+
 export type Folder = {
-  id: string
-  name: string
-  size: number
-}
- 
+  id: string;
+  name: string;
+  size: number;
+};
+
 export const Column: ColumnDef<Folder>[] = [
   {
     accessorKey: "name",
@@ -17,4 +26,33 @@ export const Column: ColumnDef<Folder>[] = [
     accessorKey: "size",
     header: "Total size",
   },
-]
+  {
+    id: "actions",
+    header:"Action",
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <HiOutlineDotsVertical className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              View
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Rename</DropdownMenuItem>
+            <DropdownMenuItem>Share</DropdownMenuItem>
+            <DropdownMenuItem>Copy link</DropdownMenuItem>
+            <DropdownMenuItem className="text-red-600">Delete</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
