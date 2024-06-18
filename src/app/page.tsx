3a96a8abcf97b5c5cd1e69c001d16b97e9cdf17c
vigneshraja122 <vraja3868@gmail.com>
2024-06-18 11:50:37 +0530

    Remove the border white space

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
             <SignIn />
           </SignedOut>
         </div>
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <div className="absolute flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8 w-full h-full flex justify-center items-center">
           Alt-Store
         </p>
