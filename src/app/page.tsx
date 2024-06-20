adbe71e227c87a6ab20e82061a9b45b9d8406eaa
vigneshraja122 <vraja3868@gmail.com>
2024-06-20 12:10:53 +0530

    Added 2nd section to the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,14 @@ import { IoMdArrowDropright } from "react-icons/io";
 import { SignInButton } from "@clerk/nextjs";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Landinginput from "./(all)/components/landinginput";
+import { Dock } from "lucide-react";
+import { Spline } from "lucide-react";
+import { DollarSign } from "lucide-react";
+import { Cloud } from "lucide-react";
+import { Split } from "lucide-react";
+import { CircleHelp } from "lucide-react";
+import { GitCompareArrows } from "lucide-react";
+import { Heart } from "lucide-react";
 
 export default function Home() {
   return (
@@ -54,9 +62,77 @@ export default function Home() {
             </p>
           </div>
           <Landinginput />
-          <p className="text-lg text-black">Get Lifetime Access for 20$</p>
+          <p className="text-lg text-black">Get Lifetime Access for 20$</p>
         </div>
-        <div className="w-full h-[50rem]"></div>
+        <div className="w-full h-[30rem] flex justify-center items-center">
+          <div className=" w-full flex flex-col justify-center items-center">
+            <div className="w-full h-full flex items-center justify-center">
+              <div className="group w-[24%] h-52 border-l border-r border-b border-gray-300 p-8 space-y-4 bg-white hover:bg-gradient-to-t hover:from-gray-100 hover:to-white">
+                <Dock className="w-7 h-7" />
+                <h1 className="text-lg font-bold transition-all duration-300 group-hover:ml-1">
+                  Built for developers
+                </h1>
+                <p className="text-sm text-gray-600">
+                  Built for engineers, developers, dreamers, thinkers and doers.
+                </p>
+              </div>
+              <div className="group w-[24%] h-52 border-l border-r border-b border-gray-300 p-8 space-y-4 bg-white hover:bg-gradient-to-t hover:from-gray-100 hover:to-white">
+                <Spline className="w-7 h-7" />
+                <h1 className="text-lg font-bold transition-all duration-300 group-hover:ml-1">Ease of use</h1>
+                <p className="text-sm text-gray-600">
+                  It&#39;s as easy as using an Apple, and as expensive as buying
+                  one.
+                </p>
+              </div>
+              <div className="group w-[24%] h-52 border-l border-r border-b border-gray-300 p-8 space-y-4 bg-white hover:bg-gradient-to-t hover:from-gray-100 hover:to-white">
+                <DollarSign className="w-7 h-7" />
+                <h1 className="text-lg font-bold transition-all duration-300 group-hover:ml-1">Pricing like no other</h1>
+                <p className="text-sm text-gray-600">
+                  Our prices are best in the market. No cap, no lock, no credit
+                  card required.
+                </p>
+              </div>
+              <div className="group w-[24%] h-52 border-l border-r border-b border-gray-300 p-8 space-y-4 bg-white hover:bg-gradient-to-t hover:from-gray-100 hover:to-white">
+                <Cloud className="w-7 h-7" />
+                <h1 className="text-lg font-bold transition-all duration-300 group-hover:ml-1">100% Uptime guarantee</h1>
+                <p className="text-sm text-gray-600">
+                  We just cannot be taken down by anyone.
+                </p>
+              </div>
+            </div>
+            <div className="w-full flex items-center justify-center">
+              <div className="group w-[24%] h-52 border-l border-r border-t border-gray-300 p-8 space-y-4 bg-white hover:bg-gradient-to-b hover:from-gray-100 hover:to-white">
+                <Split className="w-7 h-7" />
+                <h1 className="text-lg font-bold transition-all duration-300 group-hover:ml-1">Multi-tenant Architecture</h1>
+                <p className="text-sm text-gray-600">
+                  You can simply share passwords instead of buying new seats
+                </p>
+              </div>
+              <div className="group w-[24%] h-52 border-l border-r border-t border-gray-300 p-8 space-y-4 bg-white hover:bg-gradient-to-b hover:from-gray-100 hover:to-white">
+                <CircleHelp className="w-7 h-7" />
+                <h1 className="text-lg font-bold transition-all duration-300 group-hover:ml-1">24/7 Customer Support</h1>
+                <p className="text-sm text-gray-600">
+                  We are available a 100% of the time. Atleast our AI Agents
+                  are.
+                </p>
+              </div>
+              <div className="group w-[24%] h-52 border-l border-r border-t border-gray-300 p-8 space-y-4 bg-white hover:bg-gradient-to-b hover:from-gray-100 hover:to-white">
+                <GitCompareArrows className="w-7 h-7" />
+                <h1 className="text-lg font-bold transition-all duration-300 group-hover:ml-1">Money back guarantee</h1>
+                <p className="text-sm text-gray-600">
+                  If you donot like EveryAI, we will convince you to like us.
+                </p>
+              </div>
+              <div className="group w-[24%] h-52 border-l border-r border-t border-gray-300 p-8 space-y-4 bg-white hover:bg-gradient-to-b hover:from-gray-100 hover:to-white">
+                <Heart className="w-7 h-7" />
+                <h1 className="text-lg font-bold transition-all duration-300 group-hover:ml-1">And everything else</h1>
+                <p className="text-sm text-gray-600">
+                  I just ran out of copy ideas. Accept my sincere apologies
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
